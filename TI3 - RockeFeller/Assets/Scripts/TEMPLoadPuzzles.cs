c46a33e8366952a5198105f009c92d98e363f639
Mari-Berdine <62489521+Mari-Berdine@users.noreply.github.com>
2026-04-19 00:05:29 -0300

    Button LoadScene

---
+++ TI3 - RockeFeller/Assets/Scripts/TEMPLoadPuzzles.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TEMPLoadPuzzles : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
