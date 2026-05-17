c98591e51a744253eb41ab53494ad4aa2a4a2020
Kai <168924521+Kai-Ofc@users.noreply.github.com>
2026-05-17 15:14:02 -0300

    Reapply "Reapply "Merge branch 'main' into Kai""
    This reverts commit 317bcd1af36baad84ea354f50300a4d2bfdaf900.

---
+++ TI3 - RockeFeller/Assets/Scripts/Games_Scripts/Cod/CodPuzzleManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CodPuzzleManager : MonoBehaviour
+{
+    public GameObject[] puzzles;
+    public int currentPuzzle;
+    public int niveisFinalizados = 0;
+    public NPCInteraction npcInteraction;
+
+    private void Start()
+    {
+        currentPuzzle = 0;
+        for (int i = 0; i < puzzles.Length; i++)
+        {
+            if (i == currentPuzzle) puzzles[i].gameObject.SetActive(true);
+            else puzzles[i].gameObject.SetActive(false);
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    public void TerminouNivel()
+    {
+        Debug.Log("Nivel Finalizado");
+        niveisFinalizados++;
+
+        if(niveisFinalizados == puzzles.Length)
+        {
+            npcInteraction.CompletePuzzle();
+        }
+    }
+}
